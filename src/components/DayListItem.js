a5166afc53be0fbc64e83df61e4dd20dd8d29ce1
ryanc337 <cordingley.ryan@gmail.com>
2019-11-25 15:31:34 -0800

    implemented format spots function to render spots open properly

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./DayListItem";
+// import { format } from "path";
+const classNames = require("classnames");
+
+
+export default function DayListItem(props) {
+  function formatSpots(props) {
+    return props.spots === 0 ? "no spots remaining"
+         : (props.spots === 1 ? "1 spot remaining"
+         : `${props.spots} spots remaining`)
+  }
+
+  const dayClass = classNames({
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className={dayClass}>{props.name}</h2>
+      <h3 className={dayClass}>{formatSpots(props)}</h3>
+    </li>
+  );
+}
+
+
