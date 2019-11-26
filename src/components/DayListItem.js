10b8dacbda58b5709b6bd3410119ee93b54fd8f7
ryanc337 <cordingley.ryan@gmail.com>
2019-11-25 16:47:41 -0800

    added use state function to apllication js

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./DayListItem";
+import "./DayListItem.scss";
 // import { format } from "path";
 const classNames = require("classnames");
 
@@ -11,15 +11,15 @@ export default function DayListItem(props) {
          : `${props.spots} spots remaining`)
   }
 
-  const dayClass = classNames({
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
 
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className={dayClass}>{props.name}</h2>
-      <h3 className={dayClass}>{formatSpots(props)}</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2>
+      <h3>{formatSpots(props)}</h3>
     </li>
   );
 }
