6f0eb0313ed335fea1de85411b9aaceedf3a3dca
ryanc337 <cordingley.ryan@gmail.com>
2019-11-25 21:15:23 -0800

    listitem is now passing props down in itemlist and daylist functions

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+const classNames = require("classnames");
+
+export default function InterviewerList(props) {
+  console.log("interviewList", props)
+  const InterviewerClass = classNames("interviewers", {
+    "interviewers__header": true,
+    "interviewers__list": true,  
+    "text--light": true
+  });
+
+  const interviewers = props.interviewers.map((interviewer) => {
+    console.log("inside map", interviewer);
+   return(<InterviewerListItem 
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.value}
+    setInterviewer={event => props.setInterviewer(interviewer.id)}
+    />
+    )
+  })
+
+return (<section className={InterviewerClass}>
+    <h4>Interviewer</h4>
+    <ul>
+      {interviewers}
+    </ul>
+  </section>)
+}
